5bcb471e5ba5945d2c6abf2973b2ac8cf43baba1
Kintsuba <bnxth107@gmail.com>
2024-09-14 03:57:56 +0900

    chore: add firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { databaseURL } from "firebase-functions/params";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   future: {
@@ -6,7 +8,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-09-14",
 
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxtjs/google-fonts"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "@nuxtjs/google-fonts", "nuxt-vuefire"],
 
   googleFonts: {
     families: {
@@ -14,5 +16,17 @@ export default defineNuxtConfig({
     },
     display: "swap",
     text: "ログボ！",
+  },
+
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyDYyN8Tl4vSpil1r1xdlTqVEDoaxzBrMMY",
+      authDomain: "misskey-login-bonus.firebaseapp.com",
+      databaseURL: "https://misskey-login-bonus.firebaseio.com",
+      projectId: "misskey-login-bonus",
+      storageBucket: "misskey-login-bonus.appspot.com",
+      messagingSenderId: "1046728037357",
+      appId: "1:1046728037357:web:b258db2dc4c5148215cde4",
+    },
   },
 });
