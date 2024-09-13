a7fe4cbcf51ee6fc5ced6801e54c4f731e4923aa
Kintsuba <bnxth107@gmail.com>
2024-09-14 02:11:37 +0900

    feat: add header

---
+++ app/components/BonusHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="bg-red-500 w-full shadow">
+    <UContainer class="h-14 flex justify-between align-middle">
+      <h1 class="flex">
+        <UButton
+          color="red"
+          variant="solid"
+          size="xl"
+          label="ログボ！"
+          to="/"
+        />
+      </h1>
+    </UContainer>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
