0445688f0d89492e6af26da39e73d52adbbecb90
Kintsuba <bnxth107@gmail.com>
2024-09-14 03:25:51 +0900

    feat: set up routing

--- app/app.vue
+++ app/app.vue
@@ -1,4 +1,16 @@
 <template>
   <BonusHeader />
-  <UContainer> </UContainer>
+  <NuxtPage />
 </template>
+
+<script setup lang="ts">
+const title = "ログボ！";
+
+useHead({
+  title,
+  htmlAttrs: {
+    lang: "ja",
+    prefix: "og: http://ogp.me/ns#",
+  },
+});
+</script>
